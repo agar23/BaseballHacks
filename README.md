d423b557d4c655dabe71c850c0420d9c951bf69c
agar23 <aaustinrivera@gmail.com>
2019-09-14 12:31:21 -0400

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# BaseballHacks
-Baseball Hacks Day Hackathon
-
-![alt text](screenshot.png "Website Screenshot")
---
+++ precache-manifest.35644df2d1f066492fde02421c824e9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95a1d63243ce18c5cfcc683795301d3",
+    "url": "/BaseballHacks/static/media/redSox.e95a1d63.png"
+  },
+  {
+    "revision": "71b820918077ae5a40f0",
+    "url": "/BaseballHacks/static/js/runtime~main.1ef6bc6b.js"
+  },
+  {
+    "revision": "c7e64df429926a135acd",
+    "url": "/BaseballHacks/static/js/main.3f7cfccb.chunk.js"
+  },
+  {
+    "revision": "0ff4ae4328ce47408f8f",
+    "url": "/BaseballHacks/static/js/2.048479de.chunk.js"
+  },
+  {
+    "revision": "c7e64df429926a135acd",
+    "url": "/BaseballHacks/static/css/main.dfbf7c59.chunk.css"
+  },
+  {
+    "revision": "0ff4ae4328ce47408f8f",
+    "url": "/BaseballHacks/static/css/2.0685a441.chunk.css"
+  },
+  {
+    "revision": "cce7568d91557e3ed087c30f6470eaf9",
+    "url": "/BaseballHacks/index.html"
+  }
+];
